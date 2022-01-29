3c463c170501091e528eeb0b1af675401207cfea
Hannah Glazier <hrayglazier@aol.com>
2022-01-29 09:41:08 -0700

    commit tree 2b6799971401b3ed7051a9461e4ff0ff9f7aa7fc

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+
+//Welcome Message
+const currentUser = "Grace Hopper";
+const welcomeMessage = `Welcome to Flatbook, '${currentUser}!`;
+
+//Excited Welcome Message
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+//shortGreeting
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
